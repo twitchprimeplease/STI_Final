cb2b40c77704a6fe69c2964ddefe9a6345f5ff50
twitchprimeplease <melanieospitia@hotmail.com>
2023-11-23 19:30:15 -0500

    EMPEZANDO CON EL SISTEMA RE REC

--- front/src/main.js
+++ front/src/main.js
@@ -3,13 +3,13 @@ const formForm = document.getElementById("formulario");
 formForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  
 
 const prefer = document.getElementById("prefer").value;
 const day = document.getElementById("day").value;
 const modal = document.getElementById("modal").value;
+const importance = document.getElementById("valoration").value;
 
-const body = {prefer, day, modal}
+const body = {prefer, day, modal, importance}
 
 sendToPythonServer(body);
 });
